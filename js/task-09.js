c1190b09f8ff51fe4cf7f80af67d59343651215a
Oksana-Marchenko11 <oksana.marchenko1988@gmail.com>
2023-06-29 20:08:53 +0300

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btn = document.querySelector(".change-color");
+const spanElem = document.querySelector(".color");
+
+const color = () => {
+  let result = getRandomHexColor();
+  document.body.style.backgroundColor = result;
+  spanElem.textContent = result;
+}
+
+btn.addEventListener('click', color);
