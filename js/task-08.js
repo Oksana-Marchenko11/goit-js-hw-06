df25e8389208e5aa7e740ead7b6a1bdd9a3b7269
Oksana-Marchenko11 <oksana.marchenko1988@gmail.com>
2023-07-01 14:28:24 +0300

    All taska done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", handleSubmit);
 
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        return alert('All fields must to be filled!');
+    }
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+    form.reset();
+}
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,29 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btnPlus = document.querySelector('[data-create]');
+const btnMinus = document.querySelector('[data-destroy]');
+const divboxes = document.getElementById('boxes');
+
+btnPlus.addEventListener('click', createBoxes);
+btnMinus.addEventListener('click', destroyBoxes);
+
+let size = 30;
+
+function createBoxes() {
+  let amount = document.querySelector('input').value;
+  for (amount; amount > 0; amount--) {
+    // let div = document.createElement("div");
+    // div.style.width = `${size}px`;
+    // div.style.height = `${size}px`;
+    // div.style.backgroundColor = getRandomHexColor();
+    // divboxes.appendChild(div);
+    divboxes.innerHTML += `<div style="width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()} ">hello</div>`;
+    size += 10;
+  }
+}
+
+function destroyBoxes() {
+  size = 30;
+  divboxes.innerHTML = "";
+}
